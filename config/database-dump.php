dd864bf0f88c3fe31d86bdfa3da42f30fc3d9429
Kekeocha Justin Chetachukwu <39213342+justinkekeocha@users.noreply.github.com>
2023-11-17 17:40:46 +0100

    chore: add commenting

--- config/database-dump.php
+++ config/database-dump.php
@@ -2,6 +2,14 @@
 
 return [
 
+    /*
+     *  Enable or disable the package.
+    */
     'enable' => true,
+
+    /*
+     *  Set the folder generated dumps should be save in.
+    */
+
     'folder' => database_path('dumps/'),
 ];
