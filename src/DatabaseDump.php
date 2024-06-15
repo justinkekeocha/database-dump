441d237486535965c0eae445158660d5f601f8bf
Justin Kekeocha <justinratzinger@gmail.com>
2024-06-15 23:43:17 +0100

    refactor: fix issue where columns contain JSON

--- src/DatabaseDump.php
+++ src/DatabaseDump.php
@@ -31,7 +31,7 @@ class DatabaseDump
             foreach ($files as $file) {
                 //Remove current directory and parent directory from listing
                 //Choose only files except folders
-                if ($file != '.' && $file != '..' && is_file($directoryPath.'/'.$file)) {
+                if ($file != '.' && $file != '..' && is_file($directoryPath . '/' . $file)) {
                     $result[] = $file;
                 }
             }
@@ -53,7 +53,7 @@ class DatabaseDump
 
         //check if the pointer is an integer
         $this->filePath = is_int($needle)
-            ? $dumpFolder.array_reverse($dumpListings)[$needle]
+            ? $dumpFolder . array_reverse($dumpListings)[$needle]
             : "$dumpFolder$needle";
 
         return $this;
@@ -99,7 +99,7 @@ class DatabaseDump
         $file = fopen($this->filePath, 'r');
 
         // Ensure the file is opened
-        if (! $file) {
+        if (!$file) {
             throw new Exception("Unable to open the file: {$this->filePath}");
         }
 
@@ -109,15 +109,20 @@ class DatabaseDump
 
         try {
 
-            while (! feof($file)) {
-                $line = stream_get_line($file, $streamLength, '}');
-                $line = trim("$line", '[,]');
-                $line = "$line}";
+            while (!feof($file)) {
+                $line = stream_get_line($file, $streamLength, '},');
+                $line = trim("$line", '[]');
+
+                if (!feof($file)) {
+                    $line = "$line}";
+                }
 
                 $this->fileOffset = ftell($file);
 
                 if ($decodedJson = json_decode($line)) {
                     yield $decodedJson;
+                } else {
+                    throw new Exception("Unable to decode the JSON string: {$line}");
                 }
             }
         } finally {
@@ -149,7 +154,7 @@ class DatabaseDump
         This ensures that subsequent seed calls on the same dump file instance don't start afresh,
         But starts gets the already saved offset for the particular table and starts reading from there
         */
-        if (! $this->schema) {
+        if (!$this->schema) {
             $this->generateSchema();
         }
 
@@ -174,7 +179,7 @@ class DatabaseDump
             );
 
             //Check header tag
-            if (! $isHeader && ! $isFooter) {
+            if (!$isHeader && !$isFooter) {
                 $rowToArray = (array) $row;
                 if (is_callable($formatRowCallback)) {
                     $rowToArray = call_user_func($formatRowCallback, $rowToArray);
